f9934315f5b779ed83a18a834e2d572b2dab0d84
Marios Nicolaides <mnico005@ucr.edu>
2019-12-10 19:25:42 -0800

    Final Project

---
+++ Headers2/Player2BT.c
@@ -0,0 +1,255 @@
+enum LocStates2 {LocStart2, LocUpdate2};
+
+unsigned char shoot = 0;
+unsigned char recv;
+unsigned char segLoc2;
+unsigned char yLoc2;
+unsigned char xLoc2;
+unsigned short y2;
+unsigned short x2;
+unsigned char lastDir2;
+//ADC x > 900 => JOYStick down => 0
+//ADC x < 200 => JOYStick up => 1
+//ADC y > 900 => JOYStick right => 2
+//ADC y < 200 => JOYStick left => 3
+int LocTick2(int state){
+	switch(state){//Transitions
+		case LocStart2:
+		state = LocUpdate2;
+		break;
+		case LocUpdate2:
+		state = LocUpdate2;
+		break;
+		default:
+		state = LocStart2;
+		break;
+	}
+	switch(state){//Actions
+		case LocStart2:
+		
+		
+		segLoc2 = 0;
+		yLoc2 = 2;
+		xLoc2 = 0;
+		y2 = 0;
+		x2 = 0;
+		lastDir2 = 2;
+		break;
+		case LocUpdate2:
+		//x2 = analog_read(2);
+		//y2 = analog_read(3);
+		//y direction
+		if (USART_HasReceived(0))
+		{
+			recv = USART_Receive(0);
+			USART_Flush(0);	
+			if (recv == 'd')
+			{
+				if(segLoc2 == 0 && yLoc2 != 5){
+					segLoc2 = 7;
+					yLoc2 ++;
+					}else if(segLoc2 != 0){
+					segLoc2 --;
+				}
+				lastDir2 = 0;
+			}else if (recv == 'u')
+			{
+				if (segLoc2 == 8)
+				{
+					segLoc2 = 0;
+					yLoc2 -- ;
+					}else if(segLoc2 == 0 && yLoc2 != 0){
+					segLoc2 ++;
+				}else if (segLoc2 > 0)
+				{
+					segLoc2 ++;
+				}
+				lastDir2 = 1;
+			}
+		
+		//x direction
+			else if (recv == 'r')
+			{
+				xLoc2 ++;
+				lastDir2 = 2;
+			}else if (recv == 'l')
+			{
+				xLoc2 --;
+				lastDir2 = 3;
+			}
+			else if (recv == 's')
+			{
+				shoot = 1;
+			}
+		}
+		break;
+		default:
+		break;
+	}
+	return state;
+}
+
+//////////////////////
+enum BuildStates2 {BuildStart2, BuildCharacter2};
+
+static unsigned char character2[] = {0x00, 0x08, 0xEB, 0x3F, 0x3F, 0xEB, 0x08, 0x00};
+static unsigned char empty2[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+static unsigned char character_top12[8]; static unsigned char character_top22[8]; static unsigned char character_top32[8]; static unsigned char character_top42[8];
+static unsigned char character_top52[8]; static unsigned char character_top62[8]; static unsigned char character_top72[8]; static unsigned char character_top82[8];
+static unsigned char* character2_top[] = {empty2, character_top12, character_top22, character_top32, character_top42, character_top52,  character_top62, character_top72, character_top82};
+
+static unsigned char character_bottom12[8];static unsigned char character_bottom22[8];static unsigned char character_bottom32[8];static unsigned char character_bottom42[8];
+static unsigned char character_bottom52[8];static unsigned char character_bottom62[8];static unsigned char character_bottom72[8];static unsigned char character_bottom82[8];
+static unsigned char* character2_bottom[] = {character2, character_bottom12, character_bottom22, character_bottom32, character_bottom42, character_bottom52, character_bottom62, character_bottom72, character_bottom82};
+
+unsigned char j2 = 0;
+unsigned char i2 = 0;
+int BuildTick2(int state){
+	switch(state){//Transitions
+		case BuildStart2:
+		state = BuildCharacter2;
+		break;
+		case BuildCharacter2:
+		state = BuildCharacter2;
+		break;
+		default:
+		state = BuildStart2;
+		break;
+	}
+	switch(state){//Actions
+		case BuildStart2:
+		//initialize bottom
+		for (j2 = 1; j2 < 9; j2++) // 9
+		{
+			for (i2=0; i2<8;i2++)//13
+			{
+				character2_bottom[j2][i2] = (character2[i2] >> j2);
+			}
+		}
+		//initialize top
+		for (j2 = 1; j2 < 9; j2++)
+		{
+			for (i2=0; i2<8;i2++)
+			{
+				character2_top[j2][i2] =  (character2[i2] << (8-j2));
+			}
+		}
+		break;
+		case BuildCharacter2:
+		//build
+		if(yLoc2 == 0){
+			lcd_setXY(0x80 + xLoc2, 0x40 + yLoc2);
+			N5110_image(character2, 8);
+			}else{
+			lcd_setXY(0x80 + xLoc2 , 0x40 + yLoc2);
+			N5110_image(character2_bottom[segLoc2], 8);
+			lcd_setXY(0x80 + xLoc2, 0x40 + yLoc2 - 1 );
+			N5110_image(character2_top[segLoc2], 8);
+		}
+		if (segLoc2 == 8)
+		{
+			segLoc2 = 0;
+			yLoc2 --;
+		}
+		break;
+		default:
+		break;
+	}
+	return state;
+}
+
+enum ProjStates2 {ProjStart2, ProjWait2, ProjLeft2, ProjRight2};
+static unsigned char projectile1[] = {0x01}; static unsigned char projectile2[] = {0x02}; static unsigned char projectile3[] = {0x04}; static unsigned char projectile4[] = {0x08};
+static unsigned char projectile5[] = {0x10}; static unsigned char projectile6[] = {0x20}; static unsigned char projectile7[] = {0x40}; static unsigned char projectile8[] = {0x80};
+static unsigned char* projectile[] = {projectile8, projectile7, projectile6, projectile5, projectile4, projectile3, projectile2, projectile1};	
+static unsigned char erase[] = {0x00};
+
+char ProjX2;
+unsigned char ProjY2;
+unsigned char ProjSeg2;
+int ProjTick2(int state){
+	switch(state){//Transitions
+		case ProjStart2:
+			state = ProjWait2;
+			break;
+		case ProjWait2:
+			if (shoot && lastDir2 == 2){
+				ProjX2 = xLoc2 + 8;
+				ProjSeg2 = segLoc2; //+ 5;
+				ProjY2 = yLoc2;
+				/*if (ProjSeg > 7)
+				{
+					ProjSeg -= 7;
+					ProjY --;
+				}*/
+				state = ProjRight2;
+			}
+			else if (shoot && lastDir2 == 3){
+				ProjX2 = xLoc2 - 1;
+				ProjSeg2 = segLoc2; //+ 5;
+				ProjY2 = yLoc2;
+				/*if (ProjSeg > 7)
+				{
+					ProjSeg -= 7;
+					ProjY --;
+				}*/
+				state = ProjLeft2;
+			}else{
+				state = ProjWait2;
+			}
+			break;
+		case ProjRight2:
+			if(ProjX2 > 83){
+				state = ProjWait2;
+			}else{
+				state = ProjRight2;
+				ProjX2 ++;
+			}
+			break;
+		case ProjLeft2:
+			if (ProjX2 == 0)
+			{
+				state = ProjWait2;
+			}
+			else {
+				state = ProjLeft2;
+				ProjX2 --;
+			}
+			break;
+		default:
+			state = ProjStart2;
+			break;
+	}
+	switch(state){//Actions
+		case ProjStart2:
+			ProjX2 = 84;
+			ProjY2 = 6;
+			ProjSeg2 = 8;
+			break;
+		case ProjWait2:
+			break;
+		case ProjRight2:
+			shoot = 0;
+			lcd_setXY(0x80 + ProjX2 -1  , 0x40 + ProjY2);
+			N5110_image(erase, 1);
+			lcd_setXY(0x80 + ProjX2 , 0x40 + ProjY2);
+			N5110_image(projectile[ProjSeg2], 1);
+			break;
+		case ProjLeft2:
+			shoot = 0;
+			if (ProjX2 == 0)
+			{
+				lcd_setXY(0x80 + 1, 0x40 + ProjY2);
+				N5110_image(erase, 1);
+			}else {
+				lcd_setXY(0x80 + ProjX2  +1, 0x40 + ProjY2);
+				N5110_image(erase, 1);
+				lcd_setXY(0x80 + ProjX2 , 0x40 + ProjY2);
+				N5110_image(projectile[ProjSeg2], 1);
+			}
+			break;
+		default:
+			break;
+	}
+	return state;
+}
---
+++ Player2/Player2/main.c
@@ -0,0 +1,507 @@
+/*
+ * Player2.c
+ *
+ * Created: 11/9/2019 2:17:39 AM
+ * Author : Marios
+ */ 
+
+#include <avr/io.h>
+#include "ADC.h"
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>
+#include <timer.h>
+#include <bit.h>
+#include <scheduler.h>
+#include <io.c>
+#define F_CPU 8000000UL
+#include <util/delay.h>
+#include <string.h>
+#include "SPI_Master_C_file.c"
+#include "Font.h"
+#include "timer.h"
+#include "Nokia_5110.h"
+//#include "Player2SM.c"
+#include "usart_ATmega1284.h"
+#include "Player2BT.c"
+
+
+enum LocStates {LocStart, LocUpdate};
+
+unsigned char data;
+unsigned char segLoc;
+unsigned char yLoc;
+unsigned char xLoc;
+unsigned short y;
+unsigned short x;
+unsigned char lastDir;
+	//ADC x > 900 => JOYStick down => 0
+	//ADC x < 200 => JOYStick up => 1
+	//ADC y > 900 => JOYStick right => 2
+	//ADC y < 200 => JOYStick left => 3
+int LocTick(int state){
+	switch(state){//Transitions
+		case LocStart:
+			state = LocUpdate;
+			break;
+		case LocUpdate:
+			state = LocUpdate;
+			break;
+		default:
+			state = LocStart;
+			break;
+	}
+	switch(state){//Actions
+		case LocStart:
+			segLoc = 0;
+			yLoc = 2;
+			xLoc = 75;
+			y = 0;
+			x = 0;
+			lastDir = 3;
+			break;
+		case LocUpdate:
+			x = analog_read(0);
+			y = analog_read(1);
+			//y direction
+			if (x > 900)
+			{
+				data = 'd';
+				USART_Send(data, 0);
+				if(segLoc == 0 && yLoc != 5){
+					segLoc = 7;
+					yLoc ++;
+					}else if(segLoc != 0){
+					segLoc --;
+				}
+				lastDir = 0;
+			}else if (x < 200)
+			{
+				data = 'u';
+				USART_Send(data, 0);
+				if (segLoc == 8)
+				{
+					segLoc = 0;
+					yLoc -- ;
+					}else if(segLoc == 0 && yLoc != 0){
+					segLoc ++;
+				}else if (segLoc > 0)
+				{
+					segLoc ++;
+				}
+				lastDir = 1;
+			}
+			
+			//x direction
+			else if (y < 200 && xLoc > 0)
+			{
+				data = 'r';
+				USART_Send(data, 0);
+				xLoc ++;
+				lastDir = 2;
+			}else if ( y > 900 && xLoc < 76)
+			{
+				data = 'l';
+				USART_Send(data, 0);
+				xLoc --;
+				lastDir = 3;
+			}
+			else{
+				data = 'n';
+				USART_Send(data, 0);
+			}
+			break;
+		default:
+			break;
+	}
+	return state;
+}
+
+enum BuildStates {BuildStart, BuildCharacter}; 
+
+static unsigned char character[] = {0x00, 0x08, 0xEB, 0x3F, 0x3F, 0xEB, 0x08, 0x00};
+static unsigned char empty[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+static unsigned char character_top1[8]; static unsigned char character_top2[8]; static unsigned char character_top3[8]; static unsigned char character_top4[8];
+static unsigned char character_top5[8]; static unsigned char character_top6[8]; static unsigned char character_top7[8]; static unsigned char character_top8[8];
+static unsigned char* character_top[] = {empty, character_top1, character_top2, character_top3, character_top4, character_top5,  character_top6, character_top7, character_top8};
+
+static unsigned char character_bottom1[8];static unsigned char character_bottom2[8];static unsigned char character_bottom3[8];static unsigned char character_bottom4[8];
+static unsigned char character_bottom5[8];static unsigned char character_bottom6[8];static unsigned char character_bottom7[8];static unsigned char character_bottom8[8];
+static unsigned char* character_bottom[] = {character, character_bottom1, character_bottom2, character_bottom3, character_bottom4, character_bottom5, character_bottom6, character_bottom7, character_bottom8};
+
+unsigned char j = 0;
+unsigned char i = 0;
+int BuildTick(int state){
+	switch(state){//Transitions
+		case BuildStart:
+			state = BuildCharacter;
+			break;
+		case BuildCharacter:
+			state = BuildCharacter;
+			break;
+		default:
+			state = BuildStart;
+			break;
+	}
+	switch(state){//Actions
+		case BuildStart:
+			//initialize bottom
+			for (j = 1; j < 9; j++) // 9
+			{
+				for (i=0; i<8;i++)//13
+				{
+					character_bottom[j][i] = (character[i] >> j);
+				}
+			}
+			//initialize top
+			for (j = 1; j < 9; j++)
+			{
+				for (i=0; i<8;i++)
+				{
+					character_top[j][i] =  (character[i] << (8-j));
+				}
+			}
+			break;
+		case BuildCharacter:
+			//build
+			if(yLoc == 0){
+				lcd_setXY(0x80 + xLoc, 0x40 + yLoc);
+				N5110_image(character, 8);
+				}else{
+				lcd_setXY(0x80 + xLoc , 0x40 + yLoc);
+				N5110_image(character_bottom[segLoc], 8);
+				lcd_setXY(0x80 + xLoc, 0x40 + yLoc - 1 );
+				N5110_image(character_top[segLoc], 8);
+			}
+			if (segLoc == 8)
+			{
+				segLoc = 0;
+				yLoc --;
+			}
+			break;
+		default:
+			break;
+	}
+	return state;
+}
+
+enum ProjStates {ProjStart, ProjWait, ProjLeft, ProjRight};
+/*static unsigned char projectile1[] = {0x01}; static unsigned char projectile2[] = {0x02}; static unsigned char projectile3[] = {0x04}; static unsigned char projectile4[] = {0x08};
+static unsigned char projectile5[] = {0x10}; static unsigned char projectile6[] = {0x20}; static unsigned char projectile7[] = {0x40}; static unsigned char projectile8[] = {0x80};
+static unsigned char* projectile[] = {projectile8, projectile7, projectile6, projectile5, projectile4, projectile3, projectile2, projectile1};	
+static unsigned char erase[] = {0x00};*/
+
+char ProjX;
+unsigned char ProjY;
+unsigned char ProjSeg;
+int ProjTick(int state){
+	switch(state){//Transitions
+		case ProjStart:
+			state = ProjWait;
+			break;
+		case ProjWait:
+			if (!GetBit(PINC, 0) && lastDir == 2){
+				USART_Send('s', 0);
+				ProjX = xLoc + 8;
+				ProjSeg = segLoc; //+ 5;
+				ProjY = yLoc;
+				/*if (ProjSeg > 7)
+				{
+					ProjSeg -= 7;
+					ProjY --;
+				}*/
+				state = ProjRight;
+			}
+			else if (!GetBit(PINC, 0) && lastDir == 3){
+				USART_Send('s', 0);
+				ProjX = xLoc - 1;
+				ProjSeg = segLoc; //+ 5;
+				ProjY = yLoc;
+				/*if (ProjSeg > 7)
+				{
+					ProjSeg -= 7;
+					ProjY --;
+				}*/
+				state = ProjLeft;
+			}else{
+				state = ProjWait;
+			}
+			break;
+		case ProjRight:
+			if(ProjX > 83){
+				state = ProjWait;
+			}else{
+				state = ProjRight;
+				ProjX ++;
+			}
+			break;
+		case ProjLeft:
+			if (ProjX == 0)
+			{
+				state = ProjWait;
+			}
+			else {
+				state = ProjLeft;
+				ProjX --;
+			}
+			break;
+		default:
+			state = ProjStart;
+			break;
+	}
+	switch(state){//Actions
+		case ProjStart:
+			ProjX = 84;
+			ProjY = 6;
+			ProjSeg = 8;
+			break;
+		case ProjWait:
+			break;
+		case ProjRight:
+			lcd_setXY(0x80 + ProjX -1  , 0x40 + ProjY);
+			N5110_image(erase, 1);
+			lcd_setXY(0x80 + ProjX , 0x40 + ProjY);
+			N5110_image(projectile[ProjSeg], 1);
+			break;
+		case ProjLeft:
+			if (ProjX == 0)
+			{
+				lcd_setXY(0x80 + 1, 0x40 + ProjY);
+				N5110_image(erase, 1);
+			}else {
+				lcd_setXY(0x80 + ProjX  +1, 0x40 + ProjY);
+				N5110_image(erase, 1);
+				lcd_setXY(0x80 + ProjX , 0x40 + ProjY);
+				N5110_image(projectile[ProjSeg], 1);
+			}
+			break;
+		default:
+			break;
+	}
+	return state;
+}
+enum CollStates {collStart, collCheck, collFound};
+	
+int CollTick(int state){
+	switch(state){//Transitions
+		case collStart:
+			state = collCheck;
+			break;
+		case collCheck:
+			if (yLoc2 == ProjY && ProjSeg >= segLoc2 && ProjX < xLoc2 +8 && ProjX > xLoc2)
+			{
+				state = collFound;
+			}
+			else if (yLoc2-1 == ProjY && ProjSeg < segLoc2 && ProjX < xLoc2 +8 && ProjX > xLoc2)
+			{
+				state = collFound;
+			}
+			else{
+				state = collCheck;
+			}
+			break;
+		case collFound:
+			state = collFound;
+			break;
+		default:
+			 state = collStart;
+			 break;
+	}
+	switch(state){
+		case collStart:
+		
+			break;
+		case collCheck:
+		
+			break;
+		case collFound:
+			N5110_clear();
+			lcd_setXY(0x80, 0x40 +2);
+			N5110_Data("PLAYER 2 WINS!");
+			while(1){}
+			break;
+			
+		default:
+			break;
+	}
+	return state;
+}
+/////////////////////////////////////////
+enum CollStates2 {collStart2, collCheck2, collFound2};
+
+int CollTick2(int state){
+	switch(state){//Transitions
+		case collStart2:
+		state = collCheck2;
+		break;
+		case collCheck2:
+		if (yLoc == ProjY2 && ProjSeg2 >= segLoc && ProjX2 < xLoc +8 && ProjX2 > xLoc)
+		{
+			state = collFound2;
+		}
+		else if (yLoc-1 == ProjY2 && ProjSeg2 < segLoc && ProjX2 < xLoc +8 && ProjX2 > xLoc)
+		{
+			state = collFound2;
+		}
+		else{
+			state = collCheck2;
+		}
+		break;
+		case collFound2:
+		state = collFound2;
+		break;
+		default:
+		state = collStart2;
+		break;
+	}
+	switch(state){
+		case collStart2:
+		
+		break;
+		case collCheck2:
+		
+		break;
+		case collFound2:
+		N5110_clear();
+		lcd_setXY(0x80, 0x40 +2);
+		N5110_Data("PLAYER 1 WINS!");
+		while(1){}
+		break;
+		
+		default:
+		break;
+	}
+	return state;
+}
+int main(void)
+{
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRD = 0xFF; PORTD = 0x00;
+	DDRC = 0x00; PORTC = 0xFF;
+	//static unsigned char bottom[]= {0x00, 0x01, 0x03, 0xFF, 0x9F, 0x1F, 0xFF, 0x8F, 0x03, 0x00, 0x01, 0x00, 0x00};
+	//static unsigned char top[] =   {0xC0, 0x80, 0x00, 0x00, 0x80, 0xC0, 0xE0, 0xFF, 0xFD, 0x97, 0x97, 0x17, 0x07};
+	
+	initUSART(0);
+    ADC_init();
+	SPI_Init();
+	N5110_init();
+	N5110_clear();
+	lcd_setXY(0x9F ,0x42);
+	
+	unsigned long int Tick_Loc_calc = 75;
+	unsigned long int Tick_Build_calc = 75;
+	unsigned long int Tick_Proj_calc = 40;
+	unsigned long int Tick_Loc2_calc = 75;
+	unsigned long int Tick_Build2_calc = 75;
+	unsigned long int Tick_Proj2_calc = 40;
+	unsigned long int Tick_Coll_calc = 40;
+	
+	//Calculating GCD
+	unsigned long int tmpGCD = 1;
+	tmpGCD = findGCD(Tick_Build_calc, Tick_Loc_calc);
+	tmpGCD = findGCD(tmpGCD, Tick_Proj_calc);
+	tmpGCD = findGCD(tmpGCD, Tick_Loc2_calc);
+	tmpGCD = findGCD(tmpGCD, Tick_Build2_calc);
+	tmpGCD = findGCD(tmpGCD, Tick_Proj2_calc);
+	tmpGCD = findGCD(tmpGCD, Tick_Coll_calc);
+	//Greatest common divisor for all tasks or smallest time unit for tasks.
+	unsigned long int GCD = tmpGCD;
+	
+	//Recalculate GCD periods for scheduler
+	unsigned long int Tick_Loc_period = Tick_Loc_calc/GCD;
+	unsigned long int Tick_Build_period = Tick_Build_calc/GCD;
+	unsigned long int Tick_Proj_period = Tick_Proj_calc/GCD;
+	unsigned long int Tick_Build2_period = Tick_Build2_calc/GCD;
+	unsigned long int Tick_Loc2_period = Tick_Loc2_calc/GCD;
+	unsigned long int Tick_Proj2_period = Tick_Proj2_calc/GCD;
+	unsigned long int Tick_Coll_period = Tick_Coll_calc/GCD;
+
+	
+	static task task1;
+	static task task2;
+	static task task3;
+	static task task4;
+	static task task5;
+	static task task6;
+	static task task7;
+	static task task8;
+	
+	task *tasks[] = {&task1, &task2, &task3, &task4, &task5, &task6, &task7, &task8};
+	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+	
+	// Task 1
+	task1.state = -1;//Task initial state.
+	task1.period = Tick_Loc_period;//Task Period.
+	task1.elapsedTime = Tick_Loc_period;//Task current elapsed time.
+	task1.TickFct = &LocTick;//Function pointer for the tick.
+	
+	// Task 2
+	task2.state = -1;//Task initial state.
+	task2.period = Tick_Build_period;//Task Period.
+	task2.elapsedTime = Tick_Build_period;//Task current elapsed time.
+	task2.TickFct = &BuildTick;//Function pointer for the tick.
+	
+	// Task 3
+	task3.state = -1;//Task initial state.
+	task3.period = Tick_Proj_period;//Task Period.
+	task3.elapsedTime = Tick_Proj_period;//Task current elapsed time.
+	task3.TickFct = &ProjTick;//Function pointer for the tick.
+	
+	// Task 4
+	task4.state = -1;//Task initial state.
+	task4.period = Tick_Loc2_period;//Task Period.
+	task4.elapsedTime = Tick_Loc2_period;//Task current elapsed time.
+	task4.TickFct = &LocTick2;//Function pointer for the tick.
+	
+	// Task 5
+	task5.state = -1;//Task initial state.
+	task5.period = Tick_Build2_period;//Task Period.
+	task5.elapsedTime = Tick_Build2_period;//Task current elapsed time.
+	task5.TickFct = &BuildTick2;//Function pointer for the tick.
+	
+	// Task 6
+	task6.state = -1;//Task initial state.
+	task6.period = Tick_Proj2_period;//Task Period.
+	task6.elapsedTime = Tick_Proj2_period;//Task current elapsed time.
+	task6.TickFct = &ProjTick2;//Function pointer for the tick.
+	
+	// Task 7
+	task7.state = -1;//Task initial state.
+	task7.period = Tick_Coll_period;//Task Period.
+	task7.elapsedTime = Tick_Coll_period;//Task current elapsed time.
+	task7.TickFct = &CollTick;//Function pointer for the tick.
+	
+	// Task 8
+	task8.state = -1;//Task initial state.
+	task8.period = Tick_Coll_period;//Task Period.
+	task8.elapsedTime = Tick_Coll_period;//Task current elapsed time.
+	task8.TickFct = &CollTick2;//Function pointer for the tick.
+	
+	
+	TimerSet(GCD);
+	TimerOn();
+	
+	i=0;
+    while (1) 
+    {
+		
+
+		
+		for ( i = 0; i < numTasks; i++ ) {
+			// Task is ready to tick
+			if ( tasks[i]->elapsedTime == tasks[i]->period ) {
+				// Setting next state for task
+				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+				// Reset the elapsed time for next tick.
+				tasks[i]->elapsedTime = 0;
+			}
+			tasks[i]->elapsedTime += 1;
+		}
+				
+		while(!TimerFlag);
+		TimerFlag = 0;
+		
+		
+		
+    }
+}
+
